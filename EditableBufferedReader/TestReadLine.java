94cbc4b781f44f06bc3482fec9e708f88d7f3e52
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-01 13:16:31 +0100

    Added while loop

--- EditableBufferedReader/TestReadLine.java
+++ EditableBufferedReader/TestReadLine.java
@@ -2,18 +2,24 @@ import java.io.*;
 
 class TestReadLine {
   public static void main(String[] args) {
+
+
     EditableBufferedReader in = new EditableBufferedReader(
       new InputStreamReader(System.in));
     //String str = null;
     Integer num = 0;
-    try {
-      //str = in.readLine();
-      in.setRaw();
-            num = in.read();
-      in.unSetRaw();
-    } catch (IOException e) { e.printStackTrace(); }
-    //System.out.println("\nline is: " + str);
+    while(true){
+      try {
+        //str = in.readLine();
+        in.setRaw();
+              num = in.read();
+        in.unSetRaw();
+      } catch (IOException e) { e.printStackTrace(); }
+      //System.out.println("\nline is: " + str);
+      
+      System.out.println("\nChar is: " + num);
+    }
+   
     
-    System.out.println("\nChar is: " + num);
   }
 }
