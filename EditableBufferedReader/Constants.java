6111ef753515109a32c58c113d6d1a6db51c518f
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-01 13:16:06 +0100

    Created Constants

---
+++ EditableBufferedReader/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+        // General ASCII Codes
+        public static final int ESCAPE = 27;
+        public static final int DEL = 127;
+        public static final int BKSP = 8;
+    
+        // Custom extended ASCII Codes
+        public static final int LEFT = -1;
+        public static final int RIGHT = -2;
+        public static final int HOME = -3;
+        public static final int END = -4;
+        public static final int INS = -5;
+}
