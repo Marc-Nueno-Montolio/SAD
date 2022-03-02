7995b1710fd987a27d65673e0772a7a6fc458694
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-02 13:19:27 +0100

    Renamed Constants to Keys

---
+++ EditableBufferedReader/Keys.java
@@ -0,0 +1,16 @@
+public class Keys {
+        // General ASCII Codes
+        public static final int 
+                ESCAPE = 27,
+                DEL = 127,
+                BKSP = 8,
+                RETURN = 13;
+    
+        // Custom extended ASCII Codes
+        public static final int LEFT = -1,
+                RIGHT = -2,
+                HOME = -3,
+                END = -4,
+                INS = -5,
+                TERMINATE = -6;
+}
