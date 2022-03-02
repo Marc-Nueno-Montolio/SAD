61b7a3af3b2fbc51ea592dc17a3df79b9cdf7bc6
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-02 13:19:59 +0100

    Added add and delete functionality

--- EditableBufferedReader/Line.java
+++ EditableBufferedReader/Line.java
@@ -1,29 +1,49 @@
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
+import javax.sound.sampled.SourceDataLine;
+
 /**
  *
  * @author marcnueno
  */
-public class Line {
-    String line;
-    public int cursor;
 
-    public Line(){
+// Aquesta classe només guarda l'estat
+public class Line {
+    private String line;
+    private int cursor;
+
+    public Line() {
         line = "";
         cursor = 0;
     }
 
-    public void add (char c){
-        
-        //cursor = cursor +1;
+    public String getLine() {
+        return this.line;
     }
 
-    public void delete (){
+    public int getCursorPos() {
+        return this.cursor;
+    }
+
+    public void add(char c) {
+        this.cursor++;
+        this.line += c;
+    }
+
+    public void delete() {
+        if (this.cursor > 0) {
+            this.line = this.line.substring(0, this.line.length() - 1);
+            this.cursor--;
+        }
 
     }
 
-    public void CursorBegin (){
+    public void CursorBegin() {
         cursor = 0;
     }
-    public void CursorEnd(){
-        
+
+    public void CursorEnd() {
+
     }
 }
