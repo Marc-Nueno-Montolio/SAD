9b5f60e1630e81cb5ea04eee14f7db44d044690b
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-07 19:11:51 +0100

    Added actions

--- EditableBufferedReader/noMVC/TerminalActions.java
+++ EditableBufferedReader/noMVC/TerminalActions.java
@@ -1,6 +1,8 @@
 public class TerminalActions {
     public static final String ESCAPE = "\33",
             MOVE_LEFT = ESCAPE + "[1D",
+            MOVE_RIGHT = ESCAPE + "[1C",
             ERASE = ESCAPE + "[1P",
-            ERASE_ONE_LEFT = MOVE_LEFT + ERASE;
+            ERASE_ONE_LEFT = MOVE_LEFT + ERASE,
+            HOME = ESCAPE + "[#G";	
 }
