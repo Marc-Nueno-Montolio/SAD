532f6a8c9088439b5162195560770e31fbee42dd
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-12 20:20:42 +0100

    Changed structure to MVC

--- EditableBufferedReader/MVC/Keys.java
+++ EditableBufferedReader/MVC/Keys.java
@@ -1,18 +1,18 @@
 public class Keys {
-    // General ASCII Codes
-    public static final int 
-            ESCAPE = 27,
-            BKSP = 127,
-            RETURN = 13,
-            EXIT = 3,
-            EOT = 4;
-
-    // Custom extended ASCII Codes
-    public static final int LEFT = -1,
-            RIGHT = -2,
-            HOME = -3,
-            END = -4,
-            INS = -5,
-            TERMINATE = -6,
-            DEL = -7;
+        // General ASCII Codes
+        public static final int 
+                ESCAPE = 27,
+                BKSP = 127,
+                RETURN = 13,
+                EXIT = 3,
+                EOT = 4;
+    
+        // Custom extended ASCII Codes
+        public static final int LEFT = -1,
+                RIGHT = -2,
+                HOME = -3,
+                END = -4,
+                INS = -5,
+                TERMINATE = -6,
+                DEL = -7;
 }
