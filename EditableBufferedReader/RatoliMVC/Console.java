b09f382ac6e41c7ade8acdeef959f804c90573a9
Marc Nueno Montolio <marcnueno@gmail.com>
2022-03-17 20:14:52 +0100

    Fixed cursor position on mouse click

--- EditableBufferedReader/RatoliMVC/Console.java
+++ EditableBufferedReader/RatoliMVC/Console.java
@@ -13,45 +13,51 @@ public class Console implements Observer {
             ERASE_LINE = ESCAPE + "[2K\r",
             HOME = ESCAPE + "[1G";
 
+    
+    /** 
+     * @param o
+     * @param arg
+     */
     @Override
     public void update(Observable o, Object arg) {
-        
+
         Line line = (Line) o;
         // update the console when the observed object changes
         if (arg != null) {
             switch ((int) arg) {
+                case Keys.MB1_CLICK_DOWN:
+                    System.out.print(ESCAPE + "[" + (line.getCursorPos() + 1) + "G");
+                    break;
                 case Keys.BKSP:
                     System.out.print(ERASE_ONE_LEFT);
                     break;
                 case Keys.LEFT:
-                        System.out.print(MOVE_LEFT);
+                    System.out.print(MOVE_LEFT);
                     break;
 
                 case Keys.RIGHT:
-                        System.out.print(MOVE_RIGHT);
+                    System.out.print(MOVE_RIGHT);
                     break;
 
                 case Keys.HOME:
                     System.out.print(HOME);
                     break;
 
-                case Keys.END:                   
-                        System.out.print(ESCAPE + "[" + (line.length() - line.getCursorPos()) + "C");                
+                case Keys.END:
+                    System.out.print(ESCAPE + "[" + (line.length() - line.getCursorPos()) + "C");
                     break;
                 case Keys.INS:
                     break;
                 case Keys.DEL:
                     System.out.print(ERASE);
                     break;
-                case Keys.MB1_CLICK_DOWN:
-                    System.out.print(ESCAPE + "[" + line.getCursorPos() + "G");
-                    break;
+
             }
         } else {
-                System.out.print(ERASE_UNTIL_END);
-                System.out.print(line.toString().substring(line.getCursorPos() -1));
-                System.out.print(ESCAPE + "[" + (line.getCursorPos() + 1) + "G");
-            
+            System.out.print(ERASE_UNTIL_END);
+            System.out.print(line.toString().substring(line.getCursorPos() - 1));
+            System.out.print(ESCAPE + "[" + (line.getCursorPos() + 1) + "G");
+
         }
     }
 
