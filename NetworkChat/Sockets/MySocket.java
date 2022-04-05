2b0f1de03b694c2b696152ae17b58d53bee6f27b
Marc Nueno Montolio <marcnueno@gmail.com>
2022-04-05 19:22:02 +0200

    Created methods in MySocket class

--- NetworkChat/Sockets/MySocket.java
+++ NetworkChat/Sockets/MySocket.java
@@ -1,5 +1,38 @@
 package Sockets;
 
-public class MySocket {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MySocket extends Socket {
+    private Boolean autoFlush = true;
+    private BufferedReader br;
+    private PrintWriter pw;
+
+    public MySocket(String host, int port) throws UnknownHostException, IOException {
+        super(host, port);
+        // Initialize input & output streams:
+        this.pw = new PrintWriter(this.getOutputStream(), this.autoFlush);
+        this.br = new BufferedReader(new InputStreamReader(this.getInputStream()));
+    }
+    
+    public String readLine(){
+        try {
+            return this.br.readLine();
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void println(String line){
+        this.pw.println(line);
+    }
     
 }
